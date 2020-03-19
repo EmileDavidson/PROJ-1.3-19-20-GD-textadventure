f058db9d4c7de5a892d17c01496f1d4c578734cd
EmileDavidson <Emiledavidson395@gmail.com>
2020-03-19 13:05:43 +0100

    small changes?

--- js/script.js
+++ js/script.js
@@ -21,17 +21,18 @@ class room {
   }
 }
 let rooms = [];
-rooms[0] = new room("kantine", "room0.jpg", ["oost"], "u staat in een kantine. Hier zitten studenten te eten of computerspelletjes te doen");
-rooms[1] = new room("trap", "room1.jpg", ["west", "zuid"], "u staat op een trap naar de eerste etage. Om u heen lopen studenten omhoog en omlaag");
+rooms[0] = new room("kantine", "room0.jpg", ["oost"], "u staat in een kantine. Hier zitten studenten te eten of computerspelletjes te doen", null);
+rooms[1] = new room("trap", "room1.jpg", ["west", "zuid"], "u staat op een trap naar de eerste etage. Om u heen lopen studenten omhoog en omlaag", null);
 rooms[2] = new room("eind", "room2.jpg", ["zuid"], "u heeft gewonnen", "key");
-rooms[3] = new room("docentenkamer", "room3.jpg", ["oost"], "u staat in de lerarenkamer. De leraren eten hier hun lunch of drinken koffie of thee");
+rooms[3] = new room("docentenkamer", "room3.jpg", ["oost"], "u staat in de lerarenkamer. De leraren eten hier hun lunch of drinken koffie of thee", null);
 rooms[4] = new room("gang", "room4.jpg", ["noord", "west", "zuid"], "u staat in een gang. Studenten en leraren lopen richting de klaslokalen");
-rooms[5] = new room("medialab", "room5.jpg", ["zuid", "noord"], "u staat in het medialab. Hier kan geexperimenteerd worden met bijvoorbeeld virtual reality brillen");
-rooms[6] = new room("toiletten", "room6.jpg", ["oost"], "u staat bij de toiletten");
-rooms[7] = new room("klaslokaal", "room7.jpg", ["noord", "west", "oost"], "u staat in een klaslokaal. De tafels staan recht achter elkaar en voorin is een projector en een smartboard");
-rooms[8] = new room("examenlokaal", "room8.jpg", ["noord", "west"], "u staat in het examenlokaal. Hier zijn de vierdejaars studenten bezig met het voorbereiden van hun examen");
+rooms[5] = new room("medialab", "room5.jpg", ["zuid", "noord"], "u staat in het medialab. Hier kan geexperimenteerd worden met bijvoorbeeld virtual reality brillen", null);
+rooms[6] = new room("toiletten", "room6.jpg", ["oost"], "u staat bij de toiletten", "key");
+rooms[7] = new room("klaslokaal", "room7.jpg", ["noord", "west", "oost"], "u staat in een klaslokaal. De tafels staan recht achter elkaar en voorin is een projector en een smartboard", null);
+rooms[8] = new room("examenlokaal", "room8.jpg", ["noord", "west"], "u staat in het examenlokaal. Hier zijn de vierdejaars studenten bezig met het voorbereiden van hun examen", null);
 
 myInput.addEventListener('keydown', getInput, false);
+
 function getInput(evt) {
   if (evt.key == "Enter") {
     let inputArray = myInput.value.split(" ");
@@ -66,24 +67,29 @@ function getInput(evt) {
 
     //zorg dat je wat kan pakken wanneer er ook echt iets is
     if (inputArray[0] == "pak") {
-      if (currentLocation == "6") {
-        if(rooms[currentLocation].lootgepakt == false){
+            myInput.value = "";
+      if (rooms[currentLocation].loot != null) {
+        if (rooms[currentLocation].lootgepakt == false) {
           rooms[currentLocation].lootgepakt = true;
-        if (inventory.length <= 0) {
-          inventory[0] = "key";
+          inventory.push(rooms[currentLocation].loot);
+          console.log("gelukt");
+          console.log(inventory);
+
         } else {
-          inventory[inventory.length] = "key";
+          alert("er is geen loot meer om op te pakken");
         }
+      } else {
+        alert("er is hier geen loot en nooit geweest");
       }
-    }
-      myInput.value = "";
       updateInventory();
     }
+
+    //help command
     if (inputArray[0] == "help") {
       alert("commando's zijn: [ ga richting ], [ pak ], [ help ]")
       myInput.value = "";
     }
-    if (inputArray[0] != "ga" && inputArray[0] != "pak" && inputArray[0] != "gebruik" && inputArray[0] != "help") {
+    if (inputArray[0] != "ga" && inputArray[0] != "pak" && inputArray[0] != "help") {
       feedback.innerHTML = "mogelijke commando's zijn: ga, pak, gebruik en help";
       myInput.value = "";
       setTimeout(removeFeedback, 4000);
@@ -101,8 +107,8 @@ function giveLocation() {
     myDirections += "<li>" + rooms[currentLocation].directions[i] + "</li>";
   }
   myPossibilities.innerHTML = myDirections;
-  if(currentLocation == 6){
-    if(rooms[6].lootgepakt == false){
+  if (currentLocation == 6) {
+    if (rooms[6].lootgepakt == false) {
       alert("je kan loot pakken type ' pak ' ")
     }
   }
@@ -131,7 +137,7 @@ function updateInventory() {
 function keycontrole() {
   if (currentLocation == "5") {
     if (inventory.length <= 0) {
-      alert("Je hebt een key nodig");
+      alert("je hebt niet de juiste benodigdheden");
       myInput.value = "";
       return false;
     }
